a1212cc4d5a0ffc37943af5b1fd371430f43a36e
AKhrenova <a.khrenova@any.run>
2023-09-18 11:42:30 +0300

    add Lu0Bot script

--- Extractors/Lu0Bot/decode_strings_Lu0Bot.py
+++ Extractors/Lu0Bot/decode_strings_Lu0Bot.py
@@ -1,3 +1,16 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import  re, base64, malduck, sys
 
 def Decode_Base64(string):
@@ -82,6 +95,7 @@ def main():
     
     if len(sys.argv) < 3:
         print ("[!] usage: python decode_strings_Lu0Bot.py <path_file> <folder_output>")
+        print ("INPUT FILE: Clean file without deobfuscation")
         return
     
     SAMPLE = sys.argv[1] 
